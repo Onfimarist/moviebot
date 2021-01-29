789999748bd1676d41fa2eb8676cdb64539fab92
Jose Marcos Soto Montalvo <e24625670@miescuela.pr>
2021-01-29 16:48:33 -0400

    changes

--- commands/settings/settings.js
+++ commands/settings/settings.js
@@ -3,6 +3,7 @@ const { MessageFlags } = require("discord.js")
 module.exports = {
     name: "settings",
     run: async (client, message, args) => {
-        message.channel.send(args.slice(1).join(" "))
+        message.channel.send(args(0))
+        message.channel.send(args.slice(0))
     }
 }
