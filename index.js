50733e38fac507c899146f4dcd96afbbfa4f7ee7
Jose Marcos Soto Montalvo <e24625670@miescuela.pr>
2021-01-29 16:12:17 -0400

    changed client.commands.length to client.commands.get

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ client.on("message", async message => {
 
     if (cmd.length === 0) return;
 
-    let command = client.commands.length(cmd);
+    let command = client.commands.get(cmd);
     if (!command) command = client.commands.get(client.aliases.get(cmd));
 
     if (command)
