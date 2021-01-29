a955b423ee2adb8d93f59da813de5ff40338dd47
Jose Marcos Soto Montalvo <e24625670@miescuela.pr>
2021-01-29 17:10:13 -0400

    random

--- commands/settings/setchannel.js
+++ commands/settings/setchannel.js
@@ -4,7 +4,7 @@ module.exports = {
     run: async (client, message, args) => {
         if (message.deletable) message.delete()
 
-        let movieChannel = message.guild.channels.cache.find(c => c.name === args)
+        let movieChannel = message.guild.channels.cache.find(c => c.id === args)
 
         client.moviechannel = movieChannel
 
