4f729a0cb280377e43bf85bb8ad8dcef1278626f
Jose Marcos Soto Montalvo <e24625670@miescuela.pr>
2021-01-29 16:30:47 -0400

    more testing

--- commands/movie/movie.js
+++ commands/movie/movie.js
@@ -4,5 +4,7 @@ module.exports = {
     description: "Adds a movie to the movie poll.",
     run: async (client, message) => {
         if (message.deletable) message.delete()
+
+        message.channel.send("something")
     }
 }
