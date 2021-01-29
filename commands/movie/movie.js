78208b1151576e3719957973d2eacb256abe67b2
Jose Marcos Soto Montalvo <e24625670@miescuela.pr>
2021-01-29 16:36:28 -0400

    added args to the movie command

--- commands/movie/movie.js
+++ commands/movie/movie.js
@@ -2,7 +2,7 @@ module.exports = {
     name: "movie",
     category: "category",
     description: "Adds a movie to the movie poll.",
-    run: async (client, message) => {
+    run: async (client, message, args) => {
         if (message.deletable) message.delete()
 
         message.channel.send(args.slice(0).join(" "))
