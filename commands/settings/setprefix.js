822b274fc3ce32a6d92879d1e0392c4476abda6d
Jose Marcos Soto Montalvo <e24625670@miescuela.pr>
2021-01-29 16:56:18 -0400

    changed from args to args

--- commands/settings/setprefix.js
+++ commands/settings/setprefix.js
@@ -3,6 +3,6 @@ const { MessageFlags } = require("discord.js")
 module.exports = {
     name: "setprefix",
     run: async (client, message, args) => {
-        client.prefix = "args"
+        client.prefix = args
     }
 }
