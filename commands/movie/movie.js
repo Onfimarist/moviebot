2d7b01b7d95e46b6f40aa78aa5e95c8e8c258838
Jose Marcos Soto Montalvo <e24625670@miescuela.pr>
2021-01-29 16:23:11 -0400

    changed movie command

--- commands/movie/movie.js
+++ commands/movie/movie.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: "movie",
     run: async (message, client, args) => {
-        message.channel.send(args)
+        message.channel.send(args.slice(0).join(" "))
     }
 }
