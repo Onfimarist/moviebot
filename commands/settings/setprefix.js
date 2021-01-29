071a0e40ccda80bf13f2eec08d9463accb3014d7
Jose Marcos Soto Montalvo <e24625670@miescuela.pr>
2021-01-29 16:58:11 -0400

    added a message after prefix change

--- commands/settings/setprefix.js
+++ commands/settings/setprefix.js
@@ -1,8 +1,12 @@
-const { MessageFlags } = require("discord.js")
+const { MessageFlags, Channel } = require("discord.js")
 
 module.exports = {
     name: "setprefix",
     run: async (client, message, args) => {
+        if (message.deletable) message.delete
+
         client.prefix = args
+
+        message.channel.send("Changed prefix to: " + client.prefix)
     }
 }
