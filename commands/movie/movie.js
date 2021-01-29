b2d6f89094e4057563fa1cb25d480634fab619eb
Jose Marcos Soto Montalvo <e24625670@miescuela.pr>
2021-01-29 16:29:47 -0400

    random

--- commands/movie/movie.js
+++ commands/movie/movie.js
@@ -2,9 +2,7 @@ module.exports = {
     name: "movie",
     category: "category",
     description: "Adds a movie to the movie poll.",
-    run: async (client, message, args) => {
+    run: async (client, message) => {
         if (message.deletable) message.delete()
-
-        message.channel.send(args.slice(0).join(" "))
     }
 }
