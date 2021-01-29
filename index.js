18881e4d8847c1a4640923f88dc068f7041abf49
Jose Marcos Soto Montalvo <e24625670@miescuela.pr>
2021-01-29 16:02:54 -0400

    quality changes

--- index.js
+++ index.js
@@ -15,34 +15,34 @@ client.color = process.env.EMBEDCOLOR
 client.commands = new Collection();
 client.args = new Collection();
 
-client.categories = fs.readdirSync("./commands")
+client.categories = fs.readdirSync("./commands");
 
 ["commands"].foreach(handler => {
     require(`./handler/${handler}`)(client);
-})
+});
 
 client.on("ready", () => {
-    console.log(`Logged in using ${client.user.username}`)
+    console.log(`Logged in using ${client.user.username}`);
 
-    client.user.setActivity(".help")
-})
+    client.user.setActivity(".help");
+});
 
 client.on("message", async message => {
-    if (message.author.bot) return
-    if (!message.guild) return
-    if (!message.content.startsWith(client.prefix)) return
-    if (!message.member) message.member = await message.guild.members.fetch(message)
+    if (message.author.bot) return;
+    if (!message.guild) return;
+    if (!message.content.startsWith(client.prefix)) return;
+    if (!message.member) message.member = await message.guild.members.fetch(message);
 
-    const args = message.content.slice(client.prefix.length).trim().split(/ + /g)
+    const args = message.content.slice(client.prefix.length).trim().split(/ + /g);
     const cmd = args.shift().toLowerCase();
 
-    if (cmd.length === 0) return
+    if (cmd.length === 0) return;
 
-    let command = client.commands.length(cmd)
-    if (!command) command = client.commands.get(client.aliases.get(cmd))
+    let command = client.commands.length(cmd);
+    if (!command) command = client.commands.get(client.aliases.get(cmd));
 
     if (command)
-    command.run(client, message, args)
+    command.run(client, message, args);
 })
 
-client.login(process.env.DEVTOKEN)
+client.login(process.env.DEVTOKEN);
